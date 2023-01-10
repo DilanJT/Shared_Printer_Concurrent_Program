cb858700b5d6710764149bf913a8f0c6c5539b19
DilanJT <dicmandilan@gmail.com>
2023-01-10 21:47:57 +0530

    initialised the java code

---
+++ Jayamanne_CW_SharedPrinter/src/LaserPrinter.java
@@ -0,0 +1,17 @@
+public class LaserPrinter implements ServicePrinter{
+
+    @Override
+    public void printDocument(Document document) {
+
+    }
+
+    @Override
+    public void replaceTonerCartridge() {
+
+    }
+
+    @Override
+    public void refillPaper() {
+
+    }
+}
